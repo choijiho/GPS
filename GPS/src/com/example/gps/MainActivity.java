dd74beff3241173723ffece83fe7279566f765ca
Evangelist <Evangelist@192.168.1.4>
2013-04-13 21:09:16 +0900

    init

--- GPS/src/com/example/gps/MainActivity.java
+++ GPS/src/com/example/gps/MainActivity.java
@@ -1,15 +1,63 @@
 package com.example.gps;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	
+	private final LocationListener listener = new LocationListener() {
 
+		@Override
+		public void onLocationChanged(Location location) {
+			// TODO Auto-generated method stub
+			Log.d("jiho", "latitude : "+location.getLatitude());
+			Log.d("jiho", "longitude : "+location.getLongitude());
+//			
+//			Message.obtain(mHandler,
+//	                UPDATE_LATLNG,
+//	                location.getLatitude() + ", " +
+//	                location.getLongitude()).sendToTarget();
+//			
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			// TODO Auto-generated method stub
+			
+		}
+	};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+		        10000,          // 10-second interval.
+		        10,             // 10 meters.
+		        listener);
 	}
 
 	@Override
